9905af61623e26e3404a55942e70616f57c18bf0
Saigal <saigal.amatya@gmail.com>
2018-01-05 12:24:48 +0545

    First commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+// express middleware
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!!!</h1>');
+  res.send({
+    name: 'Saigal',
+    likes: [
+      'Biking',
+      'Cities'
+    ]
+  });
+});
+
+// creating routes
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    pageContent: 'This is an About Page'
+  });
+});
+
+app.get('/home', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'This is a Home Page',
+    welcomeMessage: 'Welcome to the Home Page'
+  });
+});
+
+// /bad - send back JSON with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
